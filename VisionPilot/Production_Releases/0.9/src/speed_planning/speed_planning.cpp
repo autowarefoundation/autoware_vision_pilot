37e4f2a4725f767ef0d930dddf50ade1ad54fa5c
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2026-02-18 15:54:52 +0300

    adding implementation of Speed Planning

--- VisionPilot/Production_Releases/0.9/src/speed_planning/speed_planning.cpp
+++ VisionPilot/Production_Releases/0.9/src/speed_planning/speed_planning.cpp
@@ -4,3 +4,82 @@
  * 
  * 
  */
+
+ #include "speed_planning/speed_planning.hpp"
+
+namespace autoware_pov::vision::speed_planning {
+
+SpeedPlanner::SpeedPlanner(
+                                       double relative_cipo_speed,
+                                       double cipo_distance,
+                                       double ego_speed,
+                                       double absolute_cipo_speed)
+    : relative_cipo_speed(relative_cipo_speed), cipo_distance(cipo_distance), 
+    ego_speed(ego_speed), absolute_cipo_speed(absolute_cipo_speed)
+{
+    std::cout << std::fixed << std::setprecision(6); // 4 decimal places
+    std::cout << "Speed planner initialized with parameters:\n"
+              << "  relative_cipo_speed: " << relative_cipo_speed << "\n"
+              << "  cipo_distance: " << cipo_distance << "\n"
+              << "  ego_speed: " << ego_speed << "\n"
+              << "  absolute_cipo_speed: " << absolute_cipo_speed << std::endl;
+}
+
+SpeedPlanner::setEgoSpeed(double ego_speed){
+    SpeedPlanner::ego_speed = ego_speed;
+}
+
+SpeedPlanner::setCIPOState(double relative_cipo_speed, double cipo_distance){
+    SpeedPlanner::relative_cipo_speed = relative_cipo_speed;
+    SpeedPlanner::cipo_distance = cipo_distance;
+}
+
+double SpeedPlanner::calcSafeRSSDistance(){
+    double cipo_absolute_speed = SpeedPlanner::relative_cipo_speed + SpeedPlanner::ego_speed
+
+    // Mobileye RSS formula
+    double safe_distance = (SpeedPlanningConstants::response_time * SpeedPlanner::ego_speed) +
+    (0.5 *SpeedPlanningConstants::a_max_accel * SpeedPlanningConstants::response_time * SpeedPlanningConstants::response_time) +
+    ((SpeedPlanner::ego_speed + SpeedPlanningConstants::response_time * SpeedPlanningConstants::a_max_accel) * 
+    (SpeedPlanner::ego_speed + SpeedPlanningConstants::response_time * SpeedPlanningConstants::a_max_accel))/(2*SpeedPlanningConstants::a_min_brake) -
+    ((cipo_absolute_speed * cipo_absolute_speed)/(2*SpeedPlanningConstants::a_max_brake))
+
+    // Ensuring that safe distance is at least one car length
+    if (safe_distance < 5.0){
+        safe_distance = 5.0;
+    }
+
+    return safe_distance
+
+}
+
+double SpeedPlanner::calcIdealDrivingSpeed();
+{   
+    double acceleration = 0.0;
+
+    // We are a safe distance away from the lead car
+    if (distance >= safe_distance * 1.1){
+        acceleration = 1.0;
+    }
+
+    // We are too close to the lead car
+    if(distance >= 0.5 * safe_distance && distance <= 0.9*safe_distance){
+        acceleration = -1.0;
+    }
+
+    // Forward Collision Warning and Aggressive Braking
+    if(distance < 0.5 * safe_distance && distance >= 0.25*safe_distance){
+        acceleration = -2.5;
+    }
+
+    // Automatic Emergency Braking
+    if(distance < 0.25){
+        acceleration = -5;
+    }
+
+    double set_speed = SpeedPlanner::ego_speed + acceleration*SpeedPlanningConstants::response_time;
+
+    return set_speed;
+
+
+} // namespace autoware_pov::vision::speed_planning
